5efb53de35861d9aa9b8e71fb28e40bed0f1b8ea
Just_a_hard <bonboncat.cycle@gmail.com>
2024-12-29 18:44:04 +0300

    regularization imlemented

--- classes.py
+++ classes.py
@@ -1,24 +1,20 @@
 import numpy as np
 
 class LinearRegression:
-    def __init__(self, learning_rate, iterations=100):
-        self.learning_rate = learning_rate
-        self.iterations = iterations
-
-    def fit(self, X, y):
+    def fit(self, X, y, learning_rate, iterations=100, L1_reg=0, L2_reg=0):
         n_samples, n_features = X.shape
         self.weights = np.zeros(n_features)
         self.bias = 0
 
-        for i in range(self.iterations):
+        for i in range(iterations):
             y_pred = np.dot(X, self.weights) + self.bias
             diff = y_pred - y
 
-            dw = np.dot(X.T, (diff)) / n_samples
+            dw = np.dot(X.T, (diff)) / n_samples + L1_reg * np.sign(self.weights) + 2 * L2_reg * self.weights
             db = np.sum(diff) / n_samples
 
-            self.weights -= self.learning_rate * dw
-            self.bias -= self.learning_rate * db
+            self.weights -= learning_rate * dw
+            self.bias -= learning_rate * db
 
     def predict(self, X):
         y_pred = np.dot(X, self.weights) + self.bias
@@ -38,9 +34,6 @@ class LinearRegression:
 
     def wape(self, y_true, y_pred):
         return np.sum(abs(y_true-y_pred))/np.sum(y_true)
-
-    def rmsle(self, y_true, y_pred, const):
-        return np.sqrt(np.mean((np.log(y_true+const)-np.log(y_pred-const))**2))
 
 class LogisticRegression:
     def __init__(self, learning_rate, threshold, iterations=100):
